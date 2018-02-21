cd8eab8b489f90cbe3e5275e9dff2d4dcb55e4e2
Tony Brix <tony@brix.ninja>
2018-02-21 14:16:34 -0600

    add formatCode test

--- spec/eslint-format-spec.js
+++ spec/eslint-format-spec.js
@@ -1,6 +1,8 @@
 /** @babel */
 
 import eslintFormat from "../lib/eslint-format";
+import path from "path";
+import {Range} from "atom";
 
 describe("eslint-format", function () {
 	beforeEach(async function () {
@@ -20,6 +22,17 @@ describe("eslint-format", function () {
 			const {grammars} = eslintFormat.provideCodeFormat();
 
 			expect(scopes).toEqual(grammars);
+		});
+	});
+
+	describe("formatCode", function () {
+		it("should create a new worker", async function () {
+			const simpleFile = path.join(__dirname, "fixtures", "simple.js");
+			const editor = await atom.workspace.open(simpleFile);
+			const range = new Range([0, 0], [0, 9]);
+			eslintFormat.provideCodeFormat().formatCode(editor, range);
+
+			expect(eslintFormat.workers.size).toBe(1);
 		});
 	});
 });
