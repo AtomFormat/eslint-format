0382293222f492dceff6f0a7b518458461fdb882
Tony Brix <tony@brix.ninja>
2018-02-22 08:46:00 -0600

    add projectPath spec helper

--- spec/helper.js
+++ spec/helper.js
@@ -6,4 +6,9 @@ export default {
 	fixturesPath(resolvePath = "") {
 		return path.resolve(path.join(__dirname, "fixtures"), resolvePath);
 	},
+
+	projectPath(file) {
+		const [projectPath] = (file ? atom.project.relativizePath(file) : atom.project.getPaths());
+		return projectPath;
+	}
 };
