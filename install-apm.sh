d3bd16a5c2c00623643908c03dc650dacf97ab3a
Tony Brix <tony@brix.ninja>
2019-03-31 21:53:15 -0500

    comment

--- install-apm.sh
+++ install-apm.sh
@@ -1,8 +1,7 @@
-  curl -s -L "https://atom.io/download/deb?channel=stable" \
-    -H 'Accept: application/octet-stream' \
-    -o "atom-amd64.deb"
-  /sbin/start-stop-daemon --start --quiet --pidfile /tmp/custom_xvfb_99.pid --make-pidfile --background --exec /usr/bin/Xvfb -- :99 -ac -screen 0 1280x1024x16
-  export DISPLAY=":99"
-  dpkg-deb -x atom-amd64.deb "${HOME}/atom"
-  export APM_SCRIPT_PATH="${HOME}/atom/usr/bin/"
-  export PATH="${APM_SCRIPT_PATH}:${PATH}"
+curl -s -L "https://atom.io/download/deb?channel=stable" \
+  -H 'Accept: application/octet-stream' \
+  -o "atom-amd64.deb"
+# /sbin/start-stop-daemon --start --quiet --pidfile /tmp/custom_xvfb_99.pid --make-pidfile --background --exec /usr/bin/Xvfb -- :99 -ac -screen 0 1280x1024x16
+dpkg-deb -x atom-amd64.deb "${HOME}/atom"
+export APM_SCRIPT_PATH="${HOME}/atom/usr/bin/"
+export PATH="${APM_SCRIPT_PATH}:${PATH}"
